db3e96c0417dd074309eb12ca4b42ccab9f2ed50
Fred Garcia <Fred@Garcia.com>
2021-09-07 19:05:13 -0700

    first commit

---
+++ arrays.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+
+$numberList = array(267,8765,345,'5345', 345, '<h1>Hello</h1>');
+
+$array = ['hello', 12, 34, 99, "<h1>Fred</h1>"];
+
+//print_r($numberList);
+// print_r($array);
+ echo $numberList[5];
+ echo $array[0];
+    
+?>
+
+</body>
+</html>
